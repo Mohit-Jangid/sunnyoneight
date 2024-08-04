369d9f515e48a56ad0cd3e847944ccda6046d85c
Mohit Jangid <79416490+Mohit-Jangid@users.noreply.github.com>
2024-08-04 22:14:19 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://sunnyoneight.vercel.app'
+      '/api': 'https://sunnyoneight-api.vercel.app'
     }
   }
 });
